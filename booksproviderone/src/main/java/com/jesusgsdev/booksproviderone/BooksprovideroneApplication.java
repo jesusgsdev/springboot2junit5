4a7e56ec90fb70f62e200ea27ab93d83ad0e7a20
Jesus <jesusgsdev@gmail.com>
2017-10-03 13:31:20 +0100

    Added two modules to represent two different books providers. Added Mapper for help mapping between entities and dtos.

---
+++ booksproviderone/src/main/java/com/jesusgsdev/booksproviderone/BooksprovideroneApplication.java
@@ -0,0 +1,12 @@
+package com.jesusgsdev.booksproviderone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksprovideroneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksprovideroneApplication.class, args);
+	}
+}
---
+++ booksprovidertwo/src/main/java/com/jesusgsdev/booksprovidertwo/BooksprovidertwoApplication.java
@@ -0,0 +1,12 @@
+package com.jesusgsdev.booksprovidertwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksprovidertwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksprovidertwoApplication.class, args);
+	}
+}
