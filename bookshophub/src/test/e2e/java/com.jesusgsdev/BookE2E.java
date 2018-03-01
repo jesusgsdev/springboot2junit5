b52c3686d46bd170fa47b98e2ffed73cdee5658e
Jesus <jesusgsdev@gmail.com>
2018-03-01 13:21:45 +0000

    Updated SpringBoot to 2.0.0 Release version.

---
+++ bookshophub/src/test/e2e/java/com.jesusgsdev/BookE2E.java
@@ -0,0 +1,14 @@
+package com.jesusgsdev;
+
+import org.junit.jupiter.api.Test;
+
+public class BookE2E {
+
+    private final static String hubURL = "localhost:8080/";
+
+    @Test
+    public void givenAValidRequestToCreateABook_WhenWeSendTheRequest_ThenABookIsCreated(){
+
+    }
+
+}
